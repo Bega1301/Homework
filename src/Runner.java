1a7a2b0c2f02da151fff00a793f28ef6a1aeb00f
Bega1301 <begaiym13011985@gmail.com>
2019-12-08 19:09:42 +0600

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,46 @@
+public class Runner extends Thread {
+    Runner runner;
+
+    public Runner (String name, Runner runner ) {
+        this.setName(name);
+        this.runner= runner;
+    }
+
+    public Runner(String name) {
+        this.setName(name);
+    }
+     public void run () {
+         try {
+             this.sleep(3000);
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+         if (runner != null) {
+             runner.start();
+         }
+         System.out.printf("\n%s берет палочку и бежит к ", this.getName());
+         if (runner !=null) {
+             System.out.println(runner.getName() + "\n");
+
+
+             try {
+                 runner.join();
+                 this.sleep(3000);
+                 System.out.printf(
+                         "\n%s берет палочку и бежит к ", runner.getName());
+                 System.out.println(this.getName() + "\n");
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         }else {
+             System.out.println("бежит обратно \n");
+
+         }
+
+        if (this.getName().equals("Runner 1")) {
+            System.out.println(this.getName()+ "стоит с палочкой");
+        }
+
+     }
+
+}
